5ca19aff76ba6ac76c38ad4c13e1c23d8ff3b5d1
Elina <Elel0001@stud.kea.dk>
2024-02-02 10:27:55 +0100

    cleaning up

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 window.addEventListener("DOMContentLoaded", init);
 
 const beerURL = "https://api.punkapi.com/v2/beers";
-// const beerURL = "https://kea-alt-del.dk/t7/api/products";
 // Note: function og => kan være det samme i nogle sammenhænge
 
 function init() {
   console.log("init");
+
   fetch(beerURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (json) {
-      console.log("json", json);
+      showBeers(json);
     });
+}
+
+function showBeers(beerJSON) {
+  console.log("json", beerJSON);
+  console.log("første json element", beerJSON[0]);
 }
