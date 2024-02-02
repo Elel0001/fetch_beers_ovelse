08b812b27e5b298cbdaa7054c5ef3594a5a8d053
Elina <Elel0001@stud.kea.dk>
2024-02-02 10:55:48 +0100

    tilføjelse af billede, styling og tilføjelse af data

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ window.addEventListener("DOMContentLoaded", init);
 const beerURL = "https://api.punkapi.com/v2/beers";
 // Note: function og => kan være det samme i nogle sammenhænge
 
+let beerTemplate;
+let beerContainer;
+
 function init() {
   console.log("init");
+
+  beerTemplate = document.querySelector(".beer_template");
+  console.log("beerTemplate", beerTemplate);
+
+  beerContainer = document.querySelector(".beer_container");
+  console.log("beer_container", beerContainer);
 
   fetch(beerURL)
     .then(function (response) {
@@ -16,6 +25,15 @@ function init() {
 }
 
 function showBeers(beerJSON) {
-  console.log("json", beerJSON);
   console.log("første json element", beerJSON[0]);
+  //   beerJSON er kun for at navngive og hjælpe os som programmør.
+
+  let beerClone = beerTemplate.cloneNode(true).content;
+
+  beerClone.querySelector(".beer_image").src = beerJSON[0].image_url;
+  beerClone.querySelector(".beer_image").alt = `Picture of a ${beerJSON[0].name} beer`;
+  beerClone.querySelector(".beer_name").textContent = beerJSON[0].name;
+  beerClone.querySelector(".beer_tagline").textContent = beerJSON[0].tagline;
+  beerClone.querySelector(".beer_description").textContent = beerJSON[0].description;
+  beerContainer.appendChild(beerClone);
 }
