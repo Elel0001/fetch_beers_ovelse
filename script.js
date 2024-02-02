b136f81f8ca701d29da258c8eb289e42b1514252
Elina <Elel0001@stud.kea.dk>
2024-02-02 10:14:54 +0100

    template added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Let's go!");
+window.addEventListener("DOMContentLoaded", init);
+
+const beerURL = "https://api.punkapi.com/v2/beers";
+// const beerURL = "https://kea-alt-del.dk/t7/api/products";
+// Note: function og => kan være det samme i nogle sammenhænge
+
+function init() {
+  console.log("init");
+  fetch(beerURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("json", json);
+    });
+}
