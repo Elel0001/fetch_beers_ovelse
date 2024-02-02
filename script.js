bfb960e782ef29700867cb9aaebb472ed106b61e
Elina <Elel0001@stud.kea.dk>
2024-02-02 11:16:06 +0100

    tilføjelse af alle øl

--- script.js
+++ script.js
@@ -25,15 +25,16 @@ function init() {
 }
 
 function showBeers(beerJSON) {
-  console.log("første json element", beerJSON[0]);
-  //   beerJSON er kun for at navngive og hjælpe os som programmør.
+  let beerClone;
 
-  let beerClone = beerTemplate.cloneNode(true).content;
-
-  beerClone.querySelector(".beer_image").src = beerJSON[0].image_url;
-  beerClone.querySelector(".beer_image").alt = `Picture of a ${beerJSON[0].name} beer`;
-  beerClone.querySelector(".beer_name").textContent = beerJSON[0].name;
-  beerClone.querySelector(".beer_tagline").textContent = beerJSON[0].tagline;
-  beerClone.querySelector(".beer_description").textContent = beerJSON[0].description;
-  beerContainer.appendChild(beerClone);
+  beerJSON.forEach((beer) => {
+    console.log("beer", beer);
+    beerClone = beerTemplate.cloneNode(true).content;
+    beerClone.querySelector(".beer_image").src = beer.image_url;
+    beerClone.querySelector(".beer_image").alt = `Picture of a ${beer.name} beer`;
+    beerClone.querySelector(".beer_name").textContent = beer.name;
+    beerClone.querySelector(".beer_tagline").textContent = beer.tagline;
+    beerClone.querySelector(".beer_description").textContent = beer.description;
+    beerContainer.appendChild(beerClone);
+  });
 }
